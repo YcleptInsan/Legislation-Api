3c9505e42bc5366cd012b55f32cad041b9724635
Guest User <Guest@Epicodus-3D.local>
2017-07-17 16:02:28 -0700

    Add init files for list,welcome, api keys, and one service.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -5,14 +6,35 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import { masterFirebaseConfig } from '../api-keys';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { MemeberListComponent } from './memeber-list/memeber-list.component';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket,
+  messagingSenderId: masterFirebaseConfig.messagingSenderId
+};
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    MemeberListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/propublica.service.ts
@@ -0,0 +1,21 @@
+// import { PROPUBLICA_API_KEY } from './api-keys';
+import { Injectable } from '@angular/core';
+import { Headers } from '@angular/http';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class PropublicaService {
+
+  constructor(private http: Http) { }
+
+    getAllResponses() {
+      let headers = new Header('X-API-Key': 'PROPUBLICA_API_KEY');
+      headers.append('X-API-Key': 'PROPUBLICA_API_KEY');
+      this.http.get('https://api.propublica.org/congress/v1/115/senate/members.json',
+        { headers: headers }
+      }).subscribe(response => {
+        console.log(response.json());
+      });
+    }
+}
